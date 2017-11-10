f5a0bd544dcac8cab3b560108a618e1155f7fc90
Philip Fahim <philip.louis49@gmail.com>
2017-11-09 20:50:57 -0500

    Responds to every message with the users message

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var RtmClient = require('@slack/client').RtmClient; //bot object that references the RTM API
-var CLIENT_EVENTS = require('@slack/client').CLIENT_EVENTS; //events that the bot will be listening for
+var CLIENT_EVENTS = require('@slack/client').CLIENT_EVENTS; //events that i can dispatch
+var RTM_EVENTS = require('@slack/client').RTM_EVENTS; //events that the bot will be listening for
 var chalk = require('chalk')
 
 var rtm = new RtmClient(require('./secret')); // API token
@@ -15,6 +16,12 @@ rtm.on(CLIENT_EVENTS.RTM.AUTHENTICATED, (rtmStartData) => {
     console.log(`Logged in as ${rtmStartData.self.name} of team ${rtmStartData.team.name}`);
 });
 
-rtm.on(CLIENT_EVENTS.RTM.RTM_CONNECTION_OPENED, function () {
-    rtm.sendMessage("wazzup!", channel);
+// Message that Bot will post in Slack
+// rtm.on(CLIENT_EVENTS.RTM.RTM_CONNECTION_OPENED, function () {
+//     rtm.sendMessage("wazzup!", channel);  
+// });
+
+rtm.on(RTM_EVENTS.MESSAGE, function(message) {
+    if (message.channel === channel)
+        rtm.sendMessage(`I can now respond ${message.text}!!`, message.channel);
 });
