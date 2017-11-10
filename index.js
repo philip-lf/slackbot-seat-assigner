c503f68bb3d10a4978f3c3b5fac02c4241660194
Philip Fahim <philip.louis49@gmail.com>
2017-11-09 21:10:59 -0500

    responds to specific user

--- index.js
+++ index.js
@@ -23,5 +23,72 @@ rtm.on(CLIENT_EVENTS.RTM.AUTHENTICATED, (rtmStartData) => {
 
 rtm.on(RTM_EVENTS.MESSAGE, function(message) {
     if (message.channel === channel)
-        rtm.sendMessage(`I can now respond ${message.text}!!`, message.channel);
+        console.log(message)
+        // const response = message.user === 'U6DMFKVT8' ? true : false  
+        if(message.user === 'U6DMFKVT8') { // if the sender is me or not
+            rtm.sendMessage(`I can now respond with only my messages:\n ${message.text} !!`, message.channel);
+        }
 });
+
+// LearnBot APP [11:03 AM] 
+// *@channel Pairs for Auther: Authentication Data Flow*
+// Adrien Lacquemant
+// Silun Zhang
+
+// Jerry Muzsik
+// Kenny Diaz
+
+// Bobby Lux
+// Daniel Cruser
+
+// John Pepino
+// Seth Feibus
+
+// Evans Alexis Stepanov
+// Kyle Uehlein
+
+// Bryan Le
+// Philip Fahim
+
+// Samuel Yun
+// Sol Park
+
+// Hsiangkai Liu
+// Mueed Chaudhry
+
+// Alex Villarreal
+// Andrew Ziegler
+
+// Mariel Werner
+// Rian Halperin
+
+// Mitchell Stewart
+// Nicholas Galarza
+
+// Daniel Bernard
+// Simon Chan
+// Xi Qin
+
+// Arthur Swieckowski
+// Hou In Choi
+
+// Carmen Peralta
+// Eric Garnett
+
+// Andrey Ivanov
+// Veekas Shrivastava
+
+// Allison Zhao
+// Douglas Chin
+
+// Claire Liu
+// James O'Reilly
+
+// Brian Macdonald
+// Michael Romani
+
+// Eren Erdogan
+// Rohan Saigal
+
+// Aaron Wong
+// Zhe Hua
